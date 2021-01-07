8f5e85cfc237c864e58a53f5f9c32152e26882da
SirBuster33 <sirbuster333@gmail.com>
2021-01-07 22:41:06 +0100

    Added Labels to Move class but not working.

--- src/sample/Move.java
+++ src/sample/Move.java
@@ -27,7 +27,7 @@ public class Move implements Runnable {
         this.labelYaw = labelYaw;
     }
 
-    public void Move(String command, Drone drone, GraphicsContext graphics, Canvas canvas) {
+    public void Move(String command, Drone drone, GraphicsContext graphics, Canvas canvas, Label labelX, Label labelY, Label labelZ, Label labelYaw) {
         int speed = 5;
         switch (command) {
             case "takeoff":
@@ -35,44 +35,54 @@ public class Move implements Runnable {
                 drone.setY((int)canvas.getHeight() / 2);
                 drone.setX((int)canvas.getWidth() / 2);
                 resetCanvas(graphics, canvas);
+                labelZ.setText("" + drone.getRadius());
                 break;
             case "land":
                 drone.setRadius(drone.getRadius());
                 drone.setY((int)canvas.getHeight() / 2);
                 drone.setX((int)canvas.getWidth() / 2);
                 resetCanvas(graphics, canvas);
+                labelZ.setText("" + drone.getRadius());
                 break;
             case "backward":
                 drone.setY(drone.getY() + speed);
                 resetCanvas(graphics, canvas);
+                labelX.setText("" + drone.getX());
                 break;
             case "forward":
                 drone.setY(drone.getY() - speed);
                 resetCanvas(graphics, canvas);
+                labelX.setText("" + drone.getX());
                 break;
             case "left":
                 drone.setX(drone.getX() - speed);
                 resetCanvas(graphics, canvas);
+                labelY.setText("" + drone.getY());
                 break;
             case "right":
                 drone.setX(drone.getX() + speed);
                 resetCanvas(graphics, canvas);
+                labelY.setText("" + drone.getY());
                 break;
             case "up":
                 drone.setRadius(drone.getRadius() + speed);
                 resetCanvas(graphics, canvas);
+                labelZ.setText("" + drone.getRadius());
                 break;
             case "down":
                 drone.setRadius(drone.getRadius() - speed);
                 resetCanvas(graphics, canvas);
+                labelZ.setText("" + drone.getRadius());
                 break;
             case "cw":
                 drone.setY(drone.getYaw() + speed);
                 resetCanvas(graphics, canvas);
+                labelYaw.setText("" + drone.getYaw());
                 break;
             case "ccw":
                 drone.setY(drone.getYaw() - speed);
                 resetCanvas(graphics, canvas);
+                labelYaw.setText("" + drone.getYaw());
                 break;
             case "flip b":
                 // drone.setY(drone.getY() - speed);
@@ -92,7 +102,7 @@ public class Move implements Runnable {
 
     @Override
     public void run(){
-        Move(command, drone, graphics, canvas);
+        Move(command, drone, graphics, canvas, labelX, labelY, labelZ, labelYaw);
     }
 }
 
